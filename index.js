408e81ffa2714c71e89696beda766d67aec24b16
Bostonboy-93 <47620355+Bostonboy-93@users.noreply.github.com>
2019-05-01 16:55:27 -0600

    saving the game

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Slackbot = require('slackbots');
 const axios = require('axios');
-
+require('dotenv').config()
+const url = `https://api.flowroute.com/v2/portorders/portability`;
 const bot = new Slackbot({
-    token: 'xoxb-580618039601-590779891974-KvZA4oNw9KOBYcQ95XkSXZNU',
+    token: 'xoxb-580618039601-590779891974-7G5ZFm58X4rVSt1nBCsR65u9',
     name: 'portbot'
 });
 
@@ -12,11 +13,11 @@ bot.on('start', () => {
     icon_emoji: ':smiling:'
    };
 
-   bot.postMessageToChannel(
-       'portbot',
-       'Check flowroute with @portbot!',
-       params
-   );
+//    bot.postMessageToChannel(
+//        'portbot',
+//        'Check flowroute with @portbot by sending a 10 digit number',
+//        params
+//    );
 });
 
 // Error Handler
@@ -28,36 +29,64 @@ bot.on('message', data => {
         return; 
     }
 
-    handleMessage(data.text);
+    portcheck(data.text);
 });
 
-// Response to Data
-function handleMessage(message) {
-    if(message.includes('portability')) {
-        portcheck();
-    }
-    else if(message.includes('portable')) {
-        portcheck();
-    }
-    else{
-         notvalid();     
-    }
-};
+const pattern = /(?<!\d)\d{10}(?!\d)/g
+
+// const numClean = message.replace(/\D/g, ' ');
+// console.log(message)
+
+// const message = message.split(' ');
+
 // Portability Responses
-function portcheck() {
-    axios.get('http://api.yomomma.info')
+function portcheck(message) {
+    if (pattern.test(message) === false) {
+        bot.postMessageToChannel(
+            'portbot',
+            `Umm.... you didn't give me a 10 digit number`);
+        return
+    }
+
+    const numbers = message.match(pattern)
+        .map(number => "+1" + number) 
+    
+    axios({
+        method: 'post',
+        url,
+        auth: {
+          username: process.env.FLOW_ROUTE_USERNAME,
+          password: process.env.FLOW_ROUTE_PASSWORD,
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: {
+            "numbers": numbers,
+        }
+    })
     .then(res => {
-      const joke = res.data.joke;
+        console.log(res.data)
+      const portable = res.data.data.portable;
       
       const params = {
         icon_emoji: ':tada:'
        };
-    
+       let response;
+       if  (portable.length === 0)  {
+           response = `I'm sorry this is not portable`;
+       }
+       else {
+        response = `These numbers are portable: ${portable}`
+       }
        bot.postMessageToChannel(
            'portbot',
-           `Yo Momma: ${joke}`, params);
+           response, params);
+    })
+    .catch(function (error) {
+        console.log(error.response.data);
     });
-}
+};
 
 //If not valid message
 function notvalid() {
@@ -67,11 +96,19 @@ function notvalid() {
     
        bot.postMessageToChannel(
            'portbot',
-           'What would you like me to do?' ,
+           'What would you like me to do? Give me a 10 digit number to check portability.' ,
            noParam,
          );
          
 
 };
 
-
+//Axios 
+// axios({
+//     method: 'post',
+//     url: 'https://api.flowroute.com/v2/portorders/portability',
+//     auth: {
+//       username: '95811327',
+//       password: 'KVVWeZPf9YkbrPcp15gutZmAM9JD0d0o'
+//     }
+//   });
---
+++ test.js
@@ -0,0 +1,115 @@
+const axios = require('axios')
+require('dotenv').config({path: './.env'})
+
+const url = `https://api.flowroute.com/v2/portorders/portability`;
+
+const pattern = /(?<!\d)\d{10}(?!\d)/g
+
+// const numClean = message.replace(/\D/g, ' ');
+// console.log(message)
+
+// const message = message.split(' ');
+
+
+function splitMessage(message) {
+    let numberTexts = message.split(' ');
+
+    return numberTexts;
+}
+
+function cleanNumberText(numText) {
+    return numText.replace(/\D+/, '')
+}
+
+function validateNumberText(numText) {
+    return numText.length === 10
+}
+
+function getPortabilityFromFlowroute(numbers) {
+    return axios({
+        method: 'post',
+        url,
+        auth: {
+          username: process.env.FLOW_ROUTE_USERNAME,
+          password: process.env.FLOW_ROUTE_PASSWORD,
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: {
+            "numbers": numbers,
+        }
+    })
+}
+
+function process(text) {
+    let numbers = splitMessage(text);
+
+    let cleanedNumbers = [];
+    numbers.forEach(num => {
+        let cleanNum = cleanNumberText(num);
+        cleanedNumbers.push(cleanNum)
+    });
+
+
+    let validNumbers = [];
+    cleanedNumbers.forEach(num => {
+        console.log('Clean For Loop: ', num)
+        let isValid = validateNumberText(num)
+        if (isValid) {
+            validNumbers.push(num)
+        }
+    })
+
+    console.log(validNumbers);
+
+}
+
+process("123 1234-----567890")
+
+// // Portability Responses
+function portcheck(validNumbers) {
+   
+    if (validNumbers.length === 0) {
+        // you didn't give me any valid numbers
+    }
+
+    const numbers = validNumbers.map(number => "+1" + number) 
+    
+    axios({
+        method: 'post',
+        url,
+        auth: {
+          username: process.env.FLOW_ROUTE_USERNAME,
+          password: process.env.FLOW_ROUTE_PASSWORD,
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: {
+            "numbers": numbers,
+        }
+    })
+    .then(res => {
+        console.log(res.data)
+
+        // The numbers you provided: ...
+        // The numbers with incorrect format: +1123
+        // Portable Numbers: ...
+        // Hypernetwork enabled: ...
+        // Unportable: ...
+    });
+};
+
+// //If not valid message
+// function notvalid() {
+//     const noParam = {
+//         icon_emoji: ':laughing:'
+//        };
+    
+//        bot.postMessageToChannel(
+//            'portbot',
+//            'What would you like me to do? Give me a 10 digit number to check portability.' ,
+//            noParam,
+//          );     
+// };
